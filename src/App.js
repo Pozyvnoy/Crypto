4089ca0814f45993fa0b3cf2fd5973b44e7e6a46
Pozyvnoy <nikita.retina@gmail.com>
2022-07-29 16:42:16 +0300

    kinopoisk1

--- src/App.js
+++ src/App.js
@@ -9,40 +9,66 @@ import CryptoItem from './CryptoItem';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {  
-      films:[]
- };
-  }
+    this.state = {
+      films: [],
+      pageNumber: 1,
+      maxPage: 1,
 
-componentDidMount(){
-  fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=2', {
-    method: 'GET',
-    headers: {
+    };
+    this.listOfFilms = this.listOfFilms.bind(this)
+  }
+    listOfFilms(back){
+      if(back){
+        if(this.state.pageNumber>1){
+          this.setState(state => ({pageNumber: state.pageNumber - 1}))
+        }
+        else return
+      }
+      else{
+        if(this.state.pageNumber< this.state.maxPage){
+          this.setState(state => ({pageNumber: state.pageNumber + 1}))
+        }
+        else return
+      }
+      this.getFilms(this.state.pageNumber)
+    }
+    getFilms(page){
+      fetch(`https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=${page}`, {
+      method: 'GET',
+      headers: {
         'X-API-KEY': '0476947e-fcb5-4e3d-b459-129a114aaa1c',
         'Content-Type': 'application/json',
-    },
-})
-    .then(res => res.json())
-    .then(json => this.setState({films:json.films}))
+      },
+    })
+      .then(res => res.json())
+      .then(json => this.setState({ films: json.films, maxPage: json.pagesCount }))
+    }
 
- 
- 
-}
 
-       
+  componentDidMount() {
+   this.getFilms(this.state.pageNumber)
+
+  }
+
+
 
 
   render() {
     return (
       <div className="myNodeFirstApp">
-          <Nav/>
-      <div className="container">
-      {this.state.films.map(film=>
-<CryptoItem key={film.filmId} film = {film}/>
-)}
+        <Nav />
+        <div className="container">
+          {this.state.films.map(film =>
+            <CryptoItem key={film.filmId} film={film} />
+          )}
+        </div>
+            <div className="button_page_wrap">
+            <button className="page_left" onClick={() => {this.listOfFilms(true)}} >  <i className="fa-solid fa-arrow-left"></i></button>
+            <span className="page_number">{this.state.pageNumber} </span>
+            <button className="page_right"onClick={() => {this.listOfFilms(false)}} >  <i className="fa-solid fa-arrow-right"></i></button>
+            </div>
+            
       </div>
-          
-       </div>
     );
   }
 }
